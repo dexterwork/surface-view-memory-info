cb42d18b6ab43beac81c00d12ea7a893e7ddc1b3
Dexter <dexter@ourpower.com.tw>
2015-03-23 13:52:30 +0800

    scroll view

--- app/src/main/java/stu/dex/memorytest/MainActivity.java
+++ app/src/main/java/stu/dex/memorytest/MainActivity.java
@@ -4,28 +4,39 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import stu.dex.tools.MLog;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    ArrayList<Bitmap> imgList;
+    int[] imgId=new int[]{R.id.img1,R.id.img2,R.id.img3,R.id.img4,R.id.img5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-
+        imgList=new ArrayList<Bitmap>();
         pickMemory();
-        pickImage();
+
+        for(int i:imgId){
+            ImageView img=(ImageView)findViewById(i);
+            Bitmap bitmap=pickImage();
+            imgList.add(bitmap);
+            img.setImageBitmap(bitmap);
+        }
+
         pickMemory();
 
 
@@ -36,9 +47,7 @@ public class MainActivity extends ActionBarActivity {
         try {
             MLog.i(this, "open dd.png.");
             InputStream is = getAssets().open("dd.png");
-            int size = is.available();
-            is.read(new byte[size]);
-            MLog.i(this, "input stream size: " + size);
+            MLog.i(this, "input stream size: " + is.available());
             bitmap = BitmapFactory.decodeStream(is);
             is.close();
             return bitmap;
@@ -55,10 +64,11 @@ public class MainActivity extends ActionBarActivity {
 
 
         double total = Runtime.getRuntime().totalMemory();
-        MLog.i(this, "total memory: " + (total) + " bytes.");
+        MLog.i(this, "total memory: " + (total/1024) + " KB.");
+        MLog.d(this, "total memory: " + (total/1024/1024) + " MB.");
 
         double max = Runtime.getRuntime().maxMemory();
-        MLog.i(this, "max memory: " + (max / 1024 / 1024) + "MB.");
+        MLog.d(this, "max memory: " + (max / 1024 / 1024) + "MB.");
     }
 
     @Override
