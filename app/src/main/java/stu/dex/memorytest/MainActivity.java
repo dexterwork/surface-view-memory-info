a5f8ec4c485deae060b86b0c6e73555d587ae97f
Dexter <dexter@ourpower.com.tw>
2015-03-23 14:29:43 +0800

    d2

--- app/src/main/java/stu/dex/memorytest/MainActivity.java
+++ app/src/main/java/stu/dex/memorytest/MainActivity.java
@@ -2,52 +2,81 @@ package stu.dex.memorytest;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Random;
 
 import stu.dex.tools.MLog;
 
 
 public class MainActivity extends ActionBarActivity {
     ArrayList<Bitmap> imgList;
-    int[] imgId=new int[]{R.id.img1,R.id.img2,R.id.img3,R.id.img4,R.id.img5};
+    LinearLayout linearLayout;
+    private final int IMGS = 10;
+    private String[] imgs = new String[]{"country.png", "dd.png"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
 
+    private void init() {
+        linearLayout = (LinearLayout) findViewById(R.id.imgs_layout);
+        imgList = new ArrayList<Bitmap>();
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        imgList=new ArrayList<Bitmap>();
         pickMemory();
 
-        for(int i:imgId){
-            ImageView img=(ImageView)findViewById(i);
-            Bitmap bitmap=pickImage();
-            imgList.add(bitmap);
-            img.setImageBitmap(bitmap);
+        for (int i = 0; i < IMGS; i++) {
+            linearLayout.addView(getNewImageView(pickImage()));
+            linearLayout.addView(getNewTextView(i));
         }
+
 
         pickMemory();
 
 
     }
 
+    private ImageView getNewImageView(Bitmap bitmap) {
+        ImageView iv = new ImageView(this);
+        iv.setLayoutParams(params);
+        iv.setImageBitmap(bitmap);
+        return iv;
+    }
+
+    private TextView getNewTextView(int index) {
+        TextView tv = new TextView(this);
+        tv.setLayoutParams(params);
+        tv.setText("image: " + String.valueOf(index));
+        tv.setTextColor(Color.rgb(200, 200, 200));
+        return tv;
+
+    }
+
+    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
     private Bitmap pickImage() {
+        //TODO
         Bitmap bitmap = null;
+
         try {
-            MLog.i(this, "open dd.png.");
-            InputStream is = getAssets().open("dd.png");
-            MLog.i(this, "input stream size: " + is.available());
+            InputStream is = getAssets().open(getRanImgFileName());
+//            MLog.i(this, "input stream size: " + is.available());
             bitmap = BitmapFactory.decodeStream(is);
             is.close();
             return bitmap;
@@ -57,6 +86,11 @@ public class MainActivity extends ActionBarActivity {
         return null;
     }
 
+    private String getRanImgFileName() {
+        Random ran = new Random();
+        return imgs[ran.nextInt(imgs.length)];
+    }
+
 
     private void pickMemory() {
         double free = Runtime.getRuntime().freeMemory();
@@ -64,11 +98,11 @@ public class MainActivity extends ActionBarActivity {
 
 
         double total = Runtime.getRuntime().totalMemory();
-        MLog.i(this, "total memory: " + (total/1024) + " KB.");
-        MLog.d(this, "total memory: " + (total/1024/1024) + " MB.");
+        MLog.i(this, "total memory: " + (total / 1024) + " KB.");
+        MLog.w(this, "total memory: " + (total / 1024 / 1024) + " MB.");
 
         double max = Runtime.getRuntime().maxMemory();
-        MLog.d(this, "max memory: " + (max / 1024 / 1024) + "MB.");
+        MLog.w(this, "max memory: " + (max / 1024 / 1024) + "MB.");
     }
 
     @Override
