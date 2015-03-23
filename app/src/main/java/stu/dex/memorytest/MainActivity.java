7156647c82447254c0cc56f04686ea42f8aba252
Dexter <dexter@ourpower.com.tw>
2015-03-23 10:16:18 +0800

    3 functions

--- app/src/main/java/stu/dex/memorytest/MainActivity.java
+++ app/src/main/java/stu/dex/memorytest/MainActivity.java
@@ -3,6 +3,8 @@ package stu.dex.memorytest;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import stu.dex.tools.MLog;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,7 +19,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
 
-     long free=   Runtime.getRuntime().freeMemory();
+        double free = Runtime.getRuntime().freeMemory();
+        MLog.i(this, "free memory: " + free + " bytes.");
+
+
+        double total=Runtime.getRuntime().totalMemory();
+        MLog.i(this,"total memory: "+(total/1024)+" KB." );
+
+        double max=Runtime.getRuntime().maxMemory();
+        MLog.i(this,"max memory: "+(max/1024/1024)+"MB.");
+
 
     }
+
+
+
+    @Override
+    public void onBackPressed() {
+        System.exit(0);
+    }
+
 }
