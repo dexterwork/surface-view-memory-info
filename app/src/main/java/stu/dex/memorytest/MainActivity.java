c45bb3ab08377967ac1889e4dd3c597cdf42a40f
Dexter <dexter@ourpower.com.tw>
2015-03-23 11:53:40 +0800

    add img

--- app/src/main/java/stu/dex/memorytest/MainActivity.java
+++ app/src/main/java/stu/dex/memorytest/MainActivity.java
@@ -1,7 +1,11 @@
 package stu.dex.memorytest;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import stu.dex.tools.MLog;
 
@@ -19,20 +23,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
 
-        double free = Runtime.getRuntime().freeMemory();
-        MLog.i(this, "free memory: " + free + " bytes.");
+       pickMemory();
 
+        try {
+            InputStream is = getAssets().open("dd.png");
+            is.read(new byte[is.available()]);
+            BitmapFactory.decodeStream(is);
+            MLog.i(this, "size: " + is.available());
+            is.close();
 
-        double total=Runtime.getRuntime().totalMemory();
-        MLog.i(this,"total memory: "+(total/1024)+" KB." );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        double max=Runtime.getRuntime().maxMemory();
-        MLog.i(this,"max memory: "+(max/1024/1024)+"MB.");
+        pickMemory();
 
 
     }
 
 
+    private void pickMemory(){
+        double free = Runtime.getRuntime().freeMemory();
+        MLog.i(this, "free memory: " + free + " bytes.");
+
+
+        double total = Runtime.getRuntime().totalMemory();
+        MLog.i(this, "total memory: " + (total / 1024) + " KB.");
+
+        double max = Runtime.getRuntime().maxMemory();
+        MLog.i(this, "max memory: " + (max / 1024 / 1024) + "MB.");
+    }
 
     @Override
     public void onBackPressed() {
