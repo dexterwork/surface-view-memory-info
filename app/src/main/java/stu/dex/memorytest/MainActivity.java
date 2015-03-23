236f0b7e42555eb3913435faeada34b4ec8da6c0
Dexter <dexter@ourpower.com.tw>
2015-03-23 10:00:00 +0800

    init

---
+++ app/src/main/java/stu/dex/memorytest/MainActivity.java
@@ -0,0 +1,23 @@
+package stu.dex.memorytest;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+     long free=   Runtime.getRuntime().freeMemory();
+
+    }
+}
